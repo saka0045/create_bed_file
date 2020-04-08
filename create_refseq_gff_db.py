8f2254aae9400fd58f617e8c743cdc6a31b64622
m006703 <sakai.yuta@mayo.edu>
2020-04-08 10:16:09 -0500

    added finished statement at the end of create db script

--- create_refseq_gff_db.py
+++ create_refseq_gff_db.py
@@ -6,3 +6,5 @@ import gffutils
 db = gffutils.create_db("/dlmp/sandbox/cgslIS/Yuta/Illumina/hg38_bed_file/raw_data/GRCh38_latest_genomic.gff.gz",
                         dbfn='GRCh38_unsorted.db', id_spec=["ID", "Name"], force=True, keep_order=False,
                         merge_strategy='create_unique', sort_attribute_values=False)
+
+print("script finished")
