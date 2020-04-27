f37858471deb3daaa05ff214b16399346dad3469
m006703 <sakai.yuta@mayo.edu>
2020-04-27 15:46:16 -0500

    changed parse_completed_tsv_and_create_syn_file.py to create the os.syn file from the candidate transcript and not from the tsv file

--- parse_completed_tsv_and_create_syn_file.py
+++ parse_completed_tsv_and_create_syn_file.py
@@ -9,6 +9,7 @@ the os.syn file required to use Rohan's OS scripts
 import argparse
 import os
 import sys
+from parse_ncbi_file import parse_tsv_file
 
 
 def main():
@@ -35,6 +36,7 @@ def main():
     out_syn_file = open(out_syn_file_path, "w")
 
     # Go through the list of transcripts and store them in a list
+    print("Parsing through candidate transcript file")
     candidate_transcript_list = []
     for candidate_transcript in candidate_transcript_file:
         candidate_transcript = candidate_transcript.rstrip()
@@ -46,33 +48,20 @@ def main():
 
     # Parse through the Alamut style tsv file
     # Ignore header line
-    tsv_file.readline()
-    tsv_file_dict = {}
-    abnormal_transcripts = []
-    for line in tsv_file:
-        line = line.rstrip()
-        tsv_line_item = line.split("\t")
-        tsv_gene = tsv_line_item[1]
-        tsv_transcript = tsv_line_item[7]
-        # Keep track of abnormal transcripts that contain two underscores "_"
-        if tsv_transcript.count("_") >= 2:
-            if tsv_transcript not in abnormal_transcripts:
-                abnormal_transcripts.append(tsv_transcript)
-        if tsv_transcript not in tsv_file_dict.keys():
-            tsv_file_dict[tsv_transcript] = tsv_gene
+    print("Parsing through tsv file")
+    tsv_file_dict = parse_tsv_file(tsv_file)
 
     print("Number of transcripts in the candidate transcript file is " + str(len(candidate_transcript_list)))
     print("Number of transcripts in the tsv file is " + str(len(tsv_file_dict)))
 
-    if len(candidate_transcript_list) != len(tsv_file_dict):
-        print("Transcripts in candidate transcript file not in tsv file: ")
-        for transcript in candidate_transcript_list:
-            if transcript not in tsv_file_dict.keys():
-                print(transcript)
-
     # Create the out syn file
-    for tsv_transcript, tsv_gene in tsv_file_dict.items():
-        out_syn_file.write(tsv_gene + "\t" + tsv_transcript + "\n")
+    print("Creating output file")
+    for candidate_transcript in candidate_transcript_list:
+        try:
+            candidate_gene = tsv_file_dict[candidate_transcript]
+            out_syn_file.write(candidate_gene + "\t" + candidate_transcript + "\n")
+        except KeyError:
+            print(candidate_transcript + " not found in provided tsv file.")
 
     tsv_file.close()
     candidate_transcript_file.close()
